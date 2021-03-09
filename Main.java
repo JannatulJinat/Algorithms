2f58fc9d1981f22173df3e4995918c67a0f2f1c9
JannatulJinat <jinat1270@gmail.com>
2021-03-09 16:41:12 +0600

    concepts

---
+++ Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jinat
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        //String Pool
+        String s1 = "UK"; //string literal
+        String s2 = "UK";
+        String s3 = "UK";
+        String s4 = new String(s3); //[will be in heap, not in string pool]
+
+        System.out.println(s3.compareTo(s4));// 0 true //It compares strings on the basis of Unicode value of each character in the strings.
+        System.out.println(s3 == s4); //false
+        System.out.println(s1 == s2); //true 
+
+        s1.concat(" USA");
+        System.out.println(s1); //UK
+        System.out.println(s2); //UK
+        s2 = "BANGLADESH";
+        System.out.println(s2); //BANGLADESH
+        System.out.println(s3); //UK 
+
+        System.out.println(s4); //UK 
+
+    }
+}
